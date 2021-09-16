280ec596bfc9d22c0446ae5549d4dcc6dc7814d7
wwdbsh <wwdbsh@gmail.com>
2021-09-15 22:53:45 -0700

    20210915:fin-v1-back intraday-data

--- back/router/stockTimeSeries.js
+++ back/router/stockTimeSeries.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const request = require("request");
+const axios = require("axios");
 
 router.get('/', function(req, res) {
     res.send("stock time series");
     res.end();
 });
 
-router.get("/time-series-intraday-data", (req, res) => {
-    // const symbol = req.body.SYMBOL;
-    // const interval = req.body.INTERVAL;
-    // const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}min&apikey=${api_key}`;
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AFRM&interval=1min&apikey=${api_key}`;
+router.get("/time-series-intraday-data", async (req, res) => {
+    const symbol = req.body.SYMBOL;
+    const interval = req.body.INTERVAL;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}min&apikey=${api_key}`;
+    
+    const { data } = await axios.get(url);
 
-    request.get({
-        url,
-        json:true,
-        headers:{"User-Agent":"request"}
-    }, (err, res, data) => {
-        if(err){
-            console.log("Error: ", err);
-        }else if(res.statusCode !== 200){
-            console.log("Status: ", res.statusCode);
-        }else{
-            console.log(data);
-        }
+    res.status(200).json({
+        TIME_SERIES_DATA:data[`Time Series (${interval}min)`]
     });
     res.end();
 });
